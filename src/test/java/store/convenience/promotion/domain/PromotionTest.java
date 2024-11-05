a246f5e0f977d84d2425426911f9f16b1c84a3a8
param <spsummerdoc@gmail.com>
2024-11-05 18:29:56 +0900

    test(promotion): 프로모션 활성화 및 날짜 유효성 검사 테스트 로직 추가

--- src/test/java/store/convenience/promotion/domain/PromotionTest.java
+++ src/test/java/store/convenience/promotion/domain/PromotionTest.java
@@ -5,8 +5,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import store.global.util.ErrorMessage;
 
 class PromotionTest {
@@ -44,7 +48,33 @@ class PromotionTest {
                 .hasMessage(ErrorMessage.INVALID_END_DATE.getMessage());
     }
 
-    private LocalDate getDate(int month, int dayOfMonth) {
+    @ParameterizedTest
+    @DisplayName("특정한 날짜가 프로모션에 따라 반환값이 달라진다")
+    @MethodSource("providedDate")
+    void isActivePromotion(LocalDate inputValue, boolean expectResult) throws Exception {
+        // given
+        PromotionDetails details = getDetails("test", 2, 3);
+        LocalDate startDate = getDate(2, 1);
+        LocalDate endDate = getDate(3, 9);
+        Promotion promotion = new Promotion(details, startDate, endDate);
+
+        // when
+        boolean result = promotion.isActivePromotion(inputValue);
+
+        // then
+        assertThat(result).isEqualTo(expectResult);
+    }
+
+    private static Stream<Arguments> providedDate(){
+        return Stream.of(
+                Arguments.arguments(getDate(2, 1), true),
+                Arguments.arguments(getDate(3, 9), true),
+                Arguments.arguments(getDate(1, 31), false),
+                Arguments.arguments(getDate(3, 10), false)
+        );
+    }
+
+    private static LocalDate getDate(int month, int dayOfMonth) {
         return LocalDate.of(2024, month, dayOfMonth);
     }
 
